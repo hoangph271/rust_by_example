18fffd2fa8a04d631e713c882ee2cb07a9b96fed
Huy Hoàng Phan <hoangph271@gmail.com>
2020-06-12 00:11:06 +0700

    Enums

---
+++ enums/src/main.rs
@@ -0,0 +1,35 @@
+// #[derive(Debug)]
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+// #[derive(Debug)]
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+// #[derive(Debug)]
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+// impl Message {
+//     fn write(&self) {
+//         println!("{:#?}", self);
+//     }
+// }
+fn main() {
+    // * enum with value
+    // let home = IpAddr::V4(127, 0, 0, 1);
+    // let loopback = IpAddr::V6(String::from("::1"));
+    // println!("{:#?}", home);
+    // println!("{:#?}", loopback);
+
+    // // * enum with methods, different value types
+    // Message::ChangeColor(0, 0, 0).write();
+    // Message::Quit.write();
+
+    // * 
+}
