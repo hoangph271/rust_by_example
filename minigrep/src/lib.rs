6f47b362c18e15297cc51a30cc5d318496a40e65
Huy Hoàng Phan <hoangph271@gmail.com>
2020-06-19 14:51:17 +0700

    Finish minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -32,7 +32,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     };
 
     for line in results {
-        println!("{:#?}", line);
+        println!("{}", line);
     }
 
     Ok(())
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Parsing args failed: {}", err);
+        eprintln!("Parsing args failed: {}", err);
         process::exit(1);
     });
 
-    println!("{} in {}", config.command, config.filename);
+    // println!("{} in {}", config.command, config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
